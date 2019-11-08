839adb0a3c7ff2bde6343fe5608de2e5be99fed6
ChristianNunz <38340159+ChristianNunz@users.noreply.github.com>
2019-11-08 09:50:04 -0600

    Reporte de Materias Asigandas Finalizado

--- src/Views/Administrador/Reporte_MateriasAsignadas.java
+++ src/Views/Administrador/Reporte_MateriasAsignadas.java
@@ -49,10 +49,10 @@ public class Reporte_MateriasAsignadas extends javax.swing.JInternalFrame {
         
         initComponents();
         cargartabla();
-        LLenarComboM(); 
-        LLenarComboS(); 
-        LLenarComboG(); 
-        LLenarComboT(); 
+//        LLenarComboM(); 
+//        LLenarComboS(); 
+//        LLenarComboG(); 
+//        LLenarComboT(); 
         LLenarComboD();
     }
 
@@ -67,21 +67,8 @@ public class Reporte_MateriasAsignadas extends javax.swing.JInternalFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        Btn_Actualizar = new javax.swing.JButton();
+        Btn_GenerarReporte = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        Btn_Limpiar = new javax.swing.JButton();
-        lblUsuario = new javax.swing.JLabel();
-        txtbho = new javax.swing.JTextField();
-        cmb_mat = new javax.swing.JComboBox();
-        cmb_grado = new javax.swing.JComboBox();
-        lblUsuario2 = new javax.swing.JLabel();
-        cmb_secc = new javax.swing.JComboBox();
-        lblUsuario3 = new javax.swing.JLabel();
-        cmb_turno = new javax.swing.JComboBox();
-        lblUsuario4 = new javax.swing.JLabel();
-        lblUsuario5 = new javax.swing.JLabel();
-        lblUsuario1 = new javax.swing.JLabel();
-        txt_id = new javax.swing.JTextField();
         cmb_docente = new javax.swing.JComboBox();
         lblUsuario6 = new javax.swing.JLabel();
 
@@ -90,7 +77,7 @@ public class Reporte_MateriasAsignadas extends javax.swing.JInternalFrame {
         setIconifiable(true);
         setMaximizable(true);
         setResizable(true);
-        setTitle("Editar Materia");
+        setTitle("Reporte Materias");
         setToolTipText("Editar Materia");
         setAlignmentX(0.0F);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -132,149 +119,48 @@ public class Reporte_MateriasAsignadas extends javax.swing.JInternalFrame {
 
         getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(349, 72, 910, 630));
 
-        Btn_Actualizar.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        Btn_Actualizar.setText("Actualizar");
-        Btn_Actualizar.addActionListener(new java.awt.event.ActionListener() {
+        Btn_GenerarReporte.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        Btn_GenerarReporte.setText("Generar Reporte");
+        Btn_GenerarReporte.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Btn_ActualizarActionPerformed(evt);
+                Btn_GenerarReporteActionPerformed(evt);
             }
         });
-        getContentPane().add(Btn_Actualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 330, 200, -1));
+        getContentPane().add(Btn_GenerarReporte, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 110, 210, -1));
 
         jLabel1.setFont(new java.awt.Font("Arial", 0, 36)); // NOI18N
-        jLabel1.setText("Editar Materia");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 10, 320, -1));
+        jLabel1.setText("Reporte Asignación de Materias");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 10, 560, -1));
 
-        Btn_Limpiar.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        Btn_Limpiar.setText("Limpiar");
-        Btn_Limpiar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Btn_LimpiarActionPerformed(evt);
-            }
-        });
-        getContentPane().add(Btn_Limpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 360, 200, -1));
-
-        lblUsuario.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        lblUsuario.setText("Año:");
-        getContentPane().add(lblUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 110, -1, 20));
-
-        txtbho.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        getContentPane().add(txtbho, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 180, 30));
-
-        getContentPane().add(cmb_mat, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, 180, -1));
-
-        getContentPane().add(cmb_grado, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 170, 180, -1));
-
-        lblUsuario2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        lblUsuario2.setText("Grado:");
-        getContentPane().add(lblUsuario2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 170, -1, 20));
-
-        getContentPane().add(cmb_secc, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 210, 180, -1));
-
-        lblUsuario3.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        lblUsuario3.setText("Seccion:");
-        getContentPane().add(lblUsuario3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 210, -1, 20));
-
-        getContentPane().add(cmb_turno, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 250, 180, -1));
-
-        lblUsuario4.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        lblUsuario4.setText("Turno:");
-        getContentPane().add(lblUsuario4, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 250, -1, 20));
-
-        lblUsuario5.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        lblUsuario5.setText("Materia:");
-        getContentPane().add(lblUsuario5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 140, -1, 20));
-
-        lblUsuario1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        lblUsuario1.setText("Id:");
-        getContentPane().add(lblUsuario1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 70, -1, 20));
-
-        txt_id.setEditable(false);
-        txt_id.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        txt_id.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txt_idActionPerformed(evt);
-            }
-        });
-        getContentPane().add(txt_id, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 70, 180, -1));
-
-        getContentPane().add(cmb_docente, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 290, 180, -1));
+        getContentPane().add(cmb_docente, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 80, 210, -1));
 
         lblUsuario6.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         lblUsuario6.setText("Docente:");
-        getContentPane().add(lblUsuario6, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 290, -1, 20));
+        getContentPane().add(lblUsuario6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, -1, 20));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void Btn_LimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn_LimpiarActionPerformed
-       id="null";
-       String Nombrem = cmb_docente.getSelectedItem().toString();
-        GenerarReportes gr = new GenerarReportes();
-        gr.ReporteMateria(Nombrem);
-    }//GEN-LAST:event_Btn_LimpiarActionPerformed
+    private void Btn_GenerarReporteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn_GenerarReporteActionPerformed
 
-    private void Btn_ActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Btn_ActualizarActionPerformed
-      
-        
-      String NombreM = cmb_mat.getSelectedItem().toString();
-      String Grado = cmb_grado.getSelectedItem().toString();
-      String seccion = cmb_secc.getSelectedItem().toString();
-      String turno = cmb_turno.getSelectedItem().toString();
-      String docente = cmb_docente.getSelectedItem().toString();
-      String anho = txtbho.getText();
-    
-        
-         //obtiene id seleccionado 
-         BigDecimal idm = mjc.GetIdMateria(NombreM);
-         BigDecimal idg = gjc.GetIdGrado(Grado);
-         BigDecimal ids = sjc.GetIdSeccion(seccion);
-         BigDecimal idt = tjc.GetIdTurno(turno);
-         
-       try {
-           BigDecimal idd = djc.GetIdDocnete(docente);
-           MateriaGrado materiaGrado = new MateriaGrado(new BigDecimal(txt_id.getText()));
-           materiaGrado.setAnho(new BigInteger(anho));
-           materiaGrado.setIdMateria(new Materia(idm, NombreM));
-           materiaGrado.setIdGrado(new Grado(idg, Grado));
-           materiaGrado.setIdSeccion(new Seccion(ids, seccion,new BigInteger("1")));
-           materiaGrado.setIdTurno(new Turno(idt, turno));
-           materiaGrado.setIdDocente(new Docente(idd));
-           MateriaGradoJpaController.edit(materiaGrado);
-           cargartabla();
-               
-           JOptionPane.showMessageDialog(rootPane,"Se actualizocon exito.." );
-        } 
-          
-         catch (Exception e) {
-              JOptionPane.showMessageDialog(rootPane,"ha ocurrido un error" + e );
-        }
-    }//GEN-LAST:event_Btn_ActualizarActionPerformed
+     String docente = cmb_docente.getSelectedItem().toString();
+     GenerarReportes gr = new GenerarReportes();
+     gr.ReporteMateria(docente);
+    }//GEN-LAST:event_Btn_GenerarReporteActionPerformed
 
      
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
         // TODO add your handling code here:
-        id=jTable1.getValueAt(jTable1.getSelectedRow(), 0).toString();
-        txt_id.setText(jTable1.getValueAt(jTable1.getSelectedRow(), 0).toString());
-        String materia=jTable1.getValueAt(jTable1.getSelectedRow(), 1).toString();
-        String grado=jTable1.getValueAt(jTable1.getSelectedRow(), 2).toString();
-        String seccion=jTable1.getValueAt(jTable1.getSelectedRow(), 3).toString();
-        String turno=jTable1.getValueAt(jTable1.getSelectedRow(), 4).toString();
-        String anho=jTable1.getValueAt(jTable1.getSelectedRow(), 5).toString();
-        String docente=jTable1.getValueAt(jTable1.getSelectedRow(), 6).toString();
-        
-        cmb_grado.setSelectedItem(grado);
-        cmb_mat.setSelectedItem(materia);
-        cmb_secc.setSelectedItem(seccion);
-        cmb_turno.setSelectedItem(turno);
-        cmb_docente.setSelectedItem(docente);
-        txtbho.setText(anho);
+//        id=jTable1.getValueAt(jTable1.getSelectedRow(), 0).toString();
+//        txt_id.setText(jTable1.getValueAt(jTable1.getSelectedRow(), 0).toString());
+//        String materia=jTable1.getValueAt(jTable1.getSelectedRow(), 1).toString();
+//        String grado=jTable1.getValueAt(jTable1.getSelectedRow(), 2).toString();
+//        String seccion=jTable1.getValueAt(jTable1.getSelectedRow(), 3).toString();
+//        String turno=jTable1.getValueAt(jTable1.getSelectedRow(), 4).toString();
+//        String anho=jTable1.getValueAt(jTable1.getSelectedRow(), 5).toString();
+//        String docente=jTable1.getValueAt(jTable1.getSelectedRow(), 6).toString();
   
     }//GEN-LAST:event_jTable1MouseClicked
-
-    private void txt_idActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_idActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txt_idActionPerformed
     
     private void cargartabla(){
     
@@ -294,32 +180,6 @@ public class Reporte_MateriasAsignadas extends javax.swing.JInternalFrame {
            }
           
         }
-    
-     private void LLenarComboM() {
-        List<Materia> Materias = materiaJpaController.findMateriaEntities();
-        for (Materia Materia1 : Materias) {
-            cmb_mat.addItem(Materia1.getMateriaNombre());
-        }
-    }
-    private void LLenarComboS() {
-        List<Seccion> seccions = sjc.findSeccionEntities();
-      
-        for (Seccion seccion : seccions) {
-            cmb_secc.addItem(seccion.getNombreSeccion());
-        }
-    }
-    private void LLenarComboG() {
-        List<Grado> grados = gjc.findGradoEntities();
-        for (Grado grado : grados) {
-            cmb_grado.addItem(grado.getGrado());
-        }
-    }
-    private void LLenarComboT() {
-        List<Turno> turnos = tjc.findTurnoEntities();
-        for (Turno turno : turnos) {
-            cmb_turno.addItem(turno.getTurnoNombre());
-        }
-    }
     private void LLenarComboD() {
         List<Docente> docentes = djc.findDocenteEntities();
         for (Docente docente : docentes) {
@@ -330,25 +190,12 @@ public class Reporte_MateriasAsignadas extends javax.swing.JInternalFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Btn_Actualizar;
-    private javax.swing.JButton Btn_Limpiar;
+    private javax.swing.JButton Btn_GenerarReporte;
     private javax.swing.JComboBox cmb_docente;
-    private javax.swing.JComboBox cmb_grado;
-    private javax.swing.JComboBox cmb_mat;
-    private javax.swing.JComboBox cmb_secc;
-    private javax.swing.JComboBox cmb_turno;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JLabel lblUsuario;
-    private javax.swing.JLabel lblUsuario1;
-    private javax.swing.JLabel lblUsuario2;
-    private javax.swing.JLabel lblUsuario3;
-    private javax.swing.JLabel lblUsuario4;
-    private javax.swing.JLabel lblUsuario5;
     private javax.swing.JLabel lblUsuario6;
-    private javax.swing.JTextField txt_id;
-    private javax.swing.JTextField txtbho;
     // End of variables declaration//GEN-END:variables
 
 }
